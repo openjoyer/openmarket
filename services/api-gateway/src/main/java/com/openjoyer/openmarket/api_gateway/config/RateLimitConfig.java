6a39dcd9126bd0693301fb6b7007edab6caa8845
openjoyer <burlakaegor777@gmail.com>
2026-03-24 00:07:30 +0300

    v1.5 api-gateway: added rate limit config

---
+++ services/api-gateway/src/main/java/com/openjoyer/openmarket/api_gateway/config/RateLimitConfig.java
@@ -0,0 +1,24 @@
+package com.openjoyer.openmarket.api_gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimitConfig {
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getRemoteAddress())
+                .map(addr -> addr.getAddress().getHostAddress())
+                .defaultIfEmpty("unknown");
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(
+                exchange.getRequest().getHeaders().getFirst("X-User-Id")
+        ).defaultIfEmpty("anonymous");
+    }
+}
