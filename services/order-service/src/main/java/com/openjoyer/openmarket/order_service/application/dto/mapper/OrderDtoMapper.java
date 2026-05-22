36967cb99a29b25e9ee4a9817e8cc25ce3df0b07
openjoyer <burlakaegor777@gmail.com>
2026-05-23 02:07:23 +0300

    fix(order): added dto mapper

--- services/order-service/src/main/java/com/openjoyer/openmarket/order_service/application/dto/mapper/OrderDtoMapper.java
+++ services/order-service/src/main/java/com/openjoyer/openmarket/order_service/application/dto/mapper/OrderDtoMapper.java
@@ -1,13 +1,37 @@
 package com.openjoyer.openmarket.order_service.application.dto.mapper;
 
+import com.openjoyer.openmarket.order_service.application.dto.OrderItemView;
 import com.openjoyer.openmarket.order_service.application.dto.OrderView;
 import com.openjoyer.openmarket.order_service.domain.model.Order;
+import com.openjoyer.openmarket.order_service.domain.model.OrderItem;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class OrderDtoMapper {
     public static OrderView toOrderView(Order order) {
+        if (order == null) {
+            return null;
+        }
+        List<OrderItemView> items = (order.getItems() == null) ?
+                Collections.EMPTY_LIST : order.getItems().stream()
+                .map(OrderDtoMapper::toItemView)
+                .toList();
         OrderView view = new OrderView();
-        view.setUserId(order.getUserId());
+        view.setUserId(order.getId() != null ? order.getId().toString() : null);
         view.setOrderId(order.getId().toString());
-        view.setItems();
+        view.setItems(items);
+        return view;
+    }
+
+    public static OrderItemView toItemView(OrderItem item) {
+        OrderItemView view = new OrderItemView();
+        view.setSkuId(item.getSkuId());
+        view.setTitleSnapshot(item.getTitleSnapshot());
+        view.setImageSnapshot(item.getImageSnapshot());
+        view.setPrice(item.getPrice());
+        view.setQuantity(item.getQuantity());
+        return view;
     }
 }
