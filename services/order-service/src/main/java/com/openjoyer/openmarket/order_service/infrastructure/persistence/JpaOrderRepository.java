b7c5d6ed2ae98f195c8b442f7af5ea37b1a31a2a
openjoyer <burlakaegor777@gmail.com>
2026-06-05 01:41:16 +0300

    test(order): add integration tests

--- services/order-service/src/main/java/com/openjoyer/openmarket/order_service/infrastructure/persistence/JpaOrderRepository.java
+++ services/order-service/src/main/java/com/openjoyer/openmarket/order_service/infrastructure/persistence/JpaOrderRepository.java
@@ -3,6 +3,7 @@ package com.openjoyer.openmarket.order_service.infrastructure.persistence;
 import com.openjoyer.openmarket.order_service.domain.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,8 +13,8 @@ public interface JpaOrderRepository extends JpaRepository<Order, UUID> {
     List<Order> findByUserId(String userId);
 
     @Query("select o from Order o left join fetch o.items where o.userId = :userId")
-    List<Order> findByUserIdWithItems(String userId);
+    List<Order> findByUserIdWithItems(@Param("userId") String userId);
 
     @Query("select o from Order o left join fetch o.items where o.id = :id")
-    Optional<Order> findByIdWithItems(UUID userId);
+    Optional<Order> findByIdWithItems(@Param("id") UUID id);
 }
