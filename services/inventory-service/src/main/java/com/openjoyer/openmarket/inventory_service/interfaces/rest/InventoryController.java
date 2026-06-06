d1ff6b7d27821d53c52c6e1d7b535f94b36b2267
openjoyer <burlakaegor777@gmail.com>
2026-06-06 03:07:07 +0300

    chore(inventory): scaffold inventory controller endpoints

--- services/inventory-service/src/main/java/com/openjoyer/openmarket/inventory_service/interfaces/rest/InventoryController.java
+++ services/inventory-service/src/main/java/com/openjoyer/openmarket/inventory_service/interfaces/rest/InventoryController.java
@@ -1,11 +1,23 @@
 package com.openjoyer.openmarket.inventory_service.interfaces.rest;
 
+import com.openjoyer.openmarket.inventory_service.domain.model.InventoryItem;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/inventory")
 public class InventoryController {
+
+//    @PostMapping
+//    public ResponseEntity<?> createItem(@RequestBody InventoryItem inventoryItem){
+//        return null;
+//    }
+//
+//    @PatchMapping("/replenish")
+//    public ResponseEntity<?> replenish(@RequestBody InventoryItem inventoryItem){
+//
+//    }
+
 }
